4f0daefab5d6c9ae35771cbf7428f63767a94387
samuelmunyua <samuelmunyua67@gmail.com>
2025-10-03 15:33:36 +0300

    Add files via upload

---
+++ simple_c_assignment.c
@@ -0,0 +1,20 @@
+// simple program(units) 
+/* 
+Name:samuel 
+Reg:CT100/G/26198/25 
+Description:IT UNITS 
+*/ 
+ 
+#include<stdio.h> 
+int main() { 
+    printf("SEMESTER UNITS \n"); 
+    printf("Analogue electronics \n"); 
+    printf("Computer Science and Organisation \n"); 
+    printf("Fundamental of ICT \n"); 
+    printf("Introduction to computer applications \n"); 
+    printf("Essentials of Accounting \n"); 
+    printf("Communication Skills \n"); 
+    printf("Mathematics for Science \n"); 
+    printf("Introduction to programming and Alogarithms \n"); 
+    return 0; 
+  }
